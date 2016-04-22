5852cff1463397d6c5b416c35781609af40d8fc4
LorennaFrancelino <lorenna.francelino@gmail.com>
2016-04-21 21:52:07 -0300

    Update VirtualKeyboard.java

--- src/VirtualKeyboard.java
+++ src/VirtualKeyboard.java
@@ -1,9 +1,8 @@
-
-
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
@@ -14,7 +13,7 @@ import javax.swing.JPasswordField;
 public class VirtualKeyboard extends JFrame implements ActionListener
 {
 
-	private JButton b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,Backspace, go;
+	private JButton b0,b1,b2,b3,b4,Backspace,go;
 	private JPasswordField passwordField;
 	private JLabel label;
 	private String password = new String();
@@ -34,16 +33,11 @@ public class VirtualKeyboard extends JFrame implements ActionListener
 		passwordField.setEditable(true);
 		add(passwordField , BorderLayout.NORTH);
 
-		b0 = new JButton( "0" ); add(b0); b0.addActionListener(this);
-		b1 = new JButton( "1" ); add(b1); b1.addActionListener(this);
-		b2 = new JButton( "2" ); add(b2); b2.addActionListener(this);
-		b3 = new JButton( "3" ); add(b3); b3.addActionListener(this);
-		b4 = new JButton( "4" ); add(b4); b4.addActionListener(this);
-		b5 = new JButton( "5" ); add(b5); b5.addActionListener(this);
-		b6 = new JButton( "6" ); add(b6); b6.addActionListener(this);
-		b7 = new JButton( "7" ); add(b7); b7.addActionListener(this);
-		b8 = new JButton( "8" ); add(b8); b8.addActionListener(this);
-		b9 = new JButton( "9" ); add(b9); b9.addActionListener(this);
+		b0 = new JButton( "0 ou 1" ); add(b0); b0.addActionListener(this);
+		b1 = new JButton( "2 ou 3" ); add(b1); b1.addActionListener(this);
+		b2 = new JButton( "4 ou 5" ); add(b2); b2.addActionListener(this);
+		b3 = new JButton( "6 ou 7" ); add(b3); b3.addActionListener(this);
+		b4 = new JButton( "8 ou 9" ); add(b4); b4.addActionListener(this);
 
 		Backspace = new JButton("Backspace"); add(Backspace); Backspace.addActionListener(this);
 		
@@ -57,7 +51,12 @@ public class VirtualKeyboard extends JFrame implements ActionListener
 		String button = e.getActionCommand();	
 		if(button.equals("Backspace"))
 		{
-			
+			if (!( password.equals("") || password == null))   
+			{
+				password = (String) password.subSequence(0, (password.length()-1));
+				passwordField.setText(password);
+				// System.out.println(password);
+			}
 		}
 		else if (button.equals("Entrar"))
 		{
@@ -66,9 +65,9 @@ public class VirtualKeyboard extends JFrame implements ActionListener
 		else
 		{
 		// verificar aqui a qtde de char
-			password+=button;		
+			password+=button.substring(5);		
 			passwordField.setText(password);
-			System.out.println(password);
+			// System.out.println(password);
 		}
 	}
 }
